a797ff21040588cdce28ec140339eeb0f5bb6852
wc58 <2258354832@qq.com>
2020-06-07 19:31:23 +0800

    增加了对“XPathParser类”所有方法的注释

--- src/main/java/org/apache/ibatis/parsing/XPathParser.java
+++ src/main/java/org/apache/ibatis/parsing/XPathParser.java
@@ -46,10 +46,15 @@ import org.xml.sax.SAXParseException;
  */
 public class XPathParser {
 
+  //xml所对应的Document对象
   private final Document document;
+  //验证？
   private boolean validation;
+  //实体解析器
   private EntityResolver entityResolver;
+  //配置属性
   private Properties variables;
+  //解析xml
   private XPath xpath;
 
   public XPathParser(String xml) {
@@ -132,85 +137,192 @@ public class XPathParser {
     this.document = document;
   }
 
+  /**
+   * 设置属性
+   * @param variables
+   */
   public void setVariables(Properties variables) {
     this.variables = variables;
   }
 
+  /**
+   * 从本DOM树中查找指定字符串
+   * @param expression
+   * @return
+   */
   public String evalString(String expression) {
     return evalString(document, expression);
   }
 
+  /**
+   * 从指定DOM树中查找指定字符串
+   * @param root
+   * @param expression
+   * @return
+   */
   public String evalString(Object root, String expression) {
     String result = (String) evaluate(expression, root, XPathConstants.STRING);
     result = PropertyParser.parse(result, variables);
     return result;
   }
 
+  /**
+   * 从本DOM树中查找指定布尔值
+   * @param expression
+   * @return
+   */
   public Boolean evalBoolean(String expression) {
     return evalBoolean(document, expression);
   }
 
+  /**
+   * 从指定DOM树中查找指定布尔值
+   * @param root
+   * @param expression
+   * @return
+   */
   public Boolean evalBoolean(Object root, String expression) {
     return (Boolean) evaluate(expression, root, XPathConstants.BOOLEAN);
   }
-
+  /**
+   * 从本DOM树中查找指定短整型
+   * @param expression
+   * @return
+   */
   public Short evalShort(String expression) {
     return evalShort(document, expression);
   }
 
+  /**
+   * 从指定DOM树中查找指定短整型
+   * @param root
+   * @param expression
+   * @return
+   */
   public Short evalShort(Object root, String expression) {
     return Short.valueOf(evalString(root, expression));
   }
 
+  /**
+   * 从本DOM树中查找指定整型
+   * @param expression
+   * @return
+   */
   public Integer evalInteger(String expression) {
     return evalInteger(document, expression);
   }
 
+  /**
+   * 从指定DOM树中查找指定整型
+   * @param root
+   * @param expression
+   * @return
+   */
   public Integer evalInteger(Object root, String expression) {
     return Integer.valueOf(evalString(root, expression));
   }
 
+  /**
+   * 从本DOM树中查找指定长整型
+   * @param expression
+   * @return
+   */
   public Long evalLong(String expression) {
     return evalLong(document, expression);
   }
 
+  /**
+   * 从指定DOM树中查找指定长整型
+   * @param root
+   * @param expression
+   * @return
+   */
   public Long evalLong(Object root, String expression) {
     return Long.valueOf(evalString(root, expression));
   }
 
+  /**
+   * 从本DOM树中查找指定单精度浮点数
+   * @param expression
+   * @return
+   */
   public Float evalFloat(String expression) {
     return evalFloat(document, expression);
   }
 
+  /**
+   * 从指定DOM树中查找指定单精度浮点数
+   * @param root
+   * @param expression
+   * @return
+   */
   public Float evalFloat(Object root, String expression) {
     return Float.valueOf(evalString(root, expression));
   }
 
+  /**
+   * 从本DOM树中查找指定双精度浮点数
+   * @param expression
+   * @return
+   */
   public Double evalDouble(String expression) {
     return evalDouble(document, expression);
   }
 
+  /**
+   * 从指定DOM树中查找指定双精度浮点数
+   * @param root
+   * @param expression
+   * @return
+   */
   public Double evalDouble(Object root, String expression) {
     return (Double) evaluate(expression, root, XPathConstants.NUMBER);
   }
 
+  /**
+   * 从本DOM树中查找指定节点
+   * 并封装为XNode节点的集合
+   * @param expression
+   * @return
+   */
   public List<XNode> evalNodes(String expression) {
     return evalNodes(document, expression);
   }
 
+  /**
+   * 从指定DOM树中查找指定节点
+   * 并封装为XNode节点的集合
+   * @param root
+   * @param expression
+   * @return
+   */
   public List<XNode> evalNodes(Object root, String expression) {
     List<XNode> xnodes = new ArrayList<>();
     NodeList nodes = (NodeList) evaluate(expression, root, XPathConstants.NODESET);
+    //遍历，封装，保存
     for (int i = 0; i < nodes.getLength(); i++) {
       xnodes.add(new XNode(this, nodes.item(i), variables));
     }
     return xnodes;
   }
 
+  /**
+   * 从本DOM树中查找指定节点
+   * 并封装为XNode节点
+   * @param expression
+   * @return
+   */
   public XNode evalNode(String expression) {
     return evalNode(document, expression);
   }
 
+  /**
+   * 从指定DOM树中查找指定节点
+   * 并封装为XNode节点
+   * @param root
+   * @param expression
+   * @return
+   */
   public XNode evalNode(Object root, String expression) {
     Node node = (Node) evaluate(expression, root, XPathConstants.NODE);
     if (node == null) {
@@ -219,6 +331,15 @@ public class XPathParser {
     return new XNode(this, node, variables);
   }
 
+  /**
+   * 使用xpath通过表达式
+   * 在指定DOM树中查找指定的类型值
+   *
+   * @param expression
+   * @param root
+   * @param returnType
+   * @return
+   */
   private Object evaluate(String expression, Object root, QName returnType) {
     try {
       return xpath.evaluate(expression, root, returnType);
@@ -227,8 +348,12 @@ public class XPathParser {
     }
   }
 
+  /**
+   * 创建主配置文件的DOM树
+   * @param inputSource
+   * @return
+   */
   private Document createDocument(InputSource inputSource) {
-    // important: this must only be called AFTER common constructor
     try {
       DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
       factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
@@ -264,11 +389,18 @@ public class XPathParser {
     }
   }
 
+  /**
+   * 初始化参数
+   * @param validation 验证通过？
+   * @param variables 属性
+   * @param entityResolver 实体解析器
+   */
   private void commonConstructor(boolean validation, Properties variables, EntityResolver entityResolver) {
     this.validation = validation;
     this.entityResolver = entityResolver;
     this.variables = variables;
     XPathFactory factory = XPathFactory.newInstance();
+    //初始化Xpath
     this.xpath = factory.newXPath();
   }
 
