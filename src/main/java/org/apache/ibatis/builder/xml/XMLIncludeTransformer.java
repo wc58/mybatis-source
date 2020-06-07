39e34554c77639c269d86eeb54b6e342117c8019
wc58 <2258354832@qq.com>
2020-06-07 08:56:32 +0800

    增加对“XMLIncludeTransformer类”的部分注释 V1.0

--- src/main/java/org/apache/ibatis/builder/xml/XMLIncludeTransformer.java
+++ src/main/java/org/apache/ibatis/builder/xml/XMLIncludeTransformer.java
@@ -35,14 +35,28 @@ import org.w3c.dom.NodeList;
  */
 public class XMLIncludeTransformer {
 
+  //主配置类
   private final Configuration configuration;
+  //mapper建造器的助理
   private final MapperBuilderAssistant builderAssistant;
 
+  /**
+   * 初始化
+   *  主配置类
+   *   mapper建造器的助理
+   * @param configuration
+   * @param builderAssistant
+   */
   public XMLIncludeTransformer(Configuration configuration, MapperBuilderAssistant builderAssistant) {
     this.configuration = configuration;
     this.builderAssistant = builderAssistant;
   }
 
+  /**
+   * 使用文件？
+   *    创建属性对象，并且从主配置类中取出已存在的属性，并复制到刚所创建的属性对象中
+   * @param source
+   */
   public void applyIncludes(Node source) {
     Properties variablesContext = new Properties();
     Properties configurationVariables = configuration.getVariables();
@@ -51,12 +65,18 @@ public class XMLIncludeTransformer {
   }
 
   /**
-   * Recursively apply includes through all SQL fragments.
+   * 解析Include标签
    *
+   *
+   *      Include标签？：
+   *          根据id取出所对应的sql片段的节点
+   *          获取sql片段对应的属性
+   *
+   *      元素？：
+   *      解析过并且文本内容？：
    * @param source
-   *          Include node in DOM tree
    * @param variablesContext
-   *          Current context for static variables with values
+   * @param included
    */
   private void applyIncludes(Node source, final Properties variablesContext, boolean included) {
     if (source.getNodeName().equals("include")) {
@@ -91,7 +111,19 @@ public class XMLIncludeTransformer {
     }
   }
 
+  /**
+   * 查找sql片段
+   *
+   *      解析属性
+   *      设置命名空间
+   *      从主配置类中取出该id对应的sql片段
+   *      克隆一份该节点并返回
+   * @param refid id对应的值
+   * @param variables 配置类中的属性
+   * @return
+   */
   private Node findSqlFragment(String refid, Properties variables) {
+
     refid = PropertyParser.parse(refid, variables);
     refid = builderAssistant.applyCurrentNamespace(refid, true);
     try {
@@ -102,18 +134,28 @@ public class XMLIncludeTransformer {
     }
   }
 
+  /**
+   * 从指定节点查找指定名称所对应的值
+   *
+   *      查找id对应的值
+   * @param node
+   * @param name
+   * @return
+   */
   private String getStringAttribute(Node node, String name) {
     return node.getAttributes().getNamedItem(name).getNodeValue();
   }
 
   /**
-   * Read placeholders and their values from include node definition.
+   * 获取变量
    *
+   *
+   *      获取property里面的key-value，放入声明属性中
+   *      如果声明属性为空则直接返回原有的属性对象
+   *      否则将原有和声明属性合并后返回
    * @param node
-   *          Include node instance
    * @param inheritedVariablesContext
-   *          Current context used for replace variables in new variables values
-   * @return variables context from include instance (no inherited values)
+   * @return
    */
   private Properties getVariablesContext(Node node, Properties inheritedVariablesContext) {
     Map<String, String> declaredProperties = null;
